64d0e922a2985ba407264c2ebeaf8e5cb87639c4
Gustavo Denis Huaracha Cahuana <gudehu191@gmail.com>
2022-10-23 10:25:19 -0700

    Initial Commit

---
+++ HuarachaColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HuarachaColeccionDeJuegos
+//
+//  Created by Gustavo Denis Huaracha Cahuana on 10/23/22.
+//  Copyright © 2022 Gustavo Denis Huaracha Cahuana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
